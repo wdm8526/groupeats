6f6475c5a087721567357d8a98932b12f25bfb5d
wdm8526 <wdm8526@rit.edu>
2018-08-11 21:29:24 -0500

    Finished version 2.0
    Allows for multiple files to be read instead of just one.
    
    Signed-off-by: System Overlord <wdm8526@rit.edu>

--- groupeats.py
+++ groupeats.py
@@ -2,25 +2,39 @@ import random
 import sys
 
 
+# groupeats.py takes in items ranked numberically, and based on their ranking, picks randomly, but skews to the more
+# popular items. This was a garbage script written in 2 hours while doing other things.
+
+# importer takes the system arguments and returns a list of filenames
+def importer(filenames):
+    files = list()
+    filecounter = 1
+    while filecounter < len(filenames):
+        files.append(filenames[filecounter])
+        filecounter += 1
+    return files
+
+
+# stripper reads the files and returns a list of items
 def stripper(filename):
     with open(filename) as f:
         places = list()
         for line in f:
             line_split = line.split()
             counter = 0
-            max = int(line_split[1])
-            while counter < max:
+            while counter < int(line_split[1]):
                 places.append(line_split[0])
                 counter += 1
     return places
 
 
+#main runs the whole show, including printing out the chosen item.
 def main():
-    filename = sys.argv[1]
-    eats = stripper(filename)
-    max_val = len(eats) - 1
-    chosen_val = random.randint(0, max_val)
-    print(chosen_val, " ", eats[chosen_val])
+    eats = list()
+    filelist = importer(sys.argv)
+    for file in filelist:
+        eats.extend(stripper(file))
+    print(eats[random.randint(0, len(eats) - 1)])
 
 
 main()
